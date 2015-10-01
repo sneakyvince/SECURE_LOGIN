b94a6909d54f100c9894c37b8f781578f117359b
sneakyvince <Vincent@MacBook-Pro-van-Vincent.local>
2015-10-01 12:04:36 +0200

    adds login screen styling
    Not centered yet tho

--- index.php
+++ index.php
@@ -27,36 +27,72 @@ if (login_check($mysqli) == true) {
 }
 ?>
 <!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang=""> <!--<![endif]-->
 <html>
     <head>
-        <title>Secure Login: Log In</title>
-        <link rel="stylesheet" href="styles/main.css" />
+        <!-- Boilerplate -->
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+        <link rel="stylesheet" href="css/normalize.min.css">
+        <link rel="stylesheet" href="css/main.css">
+        <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+        
+        <!-- Security -->
         <script type="text/JavaScript" src="js/sha512.js"></script> 
         <script type="text/JavaScript" src="js/forms.js"></script> 
         <script src='https://www.google.com/recaptcha/api.js'></script>
     </head>
     <body>
+        <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+        <div class="header-container">
+            <header class="wrapper clearfix">
+                <h1 class="title">Secure Login</h1>
+                <nav>
+                    <ul>
+                        <li><a href="register.php">register</a></li>
+                        <li><a href="includes/logout.php">log out</a></li>
+                        <li><a href="protected_page.php"><?php  (login_check($mysqli) == true); echo htmlentities($_SESSION['username']); ?></a></li>
+                    </ul>
+                </nav>
+            </header>
+        </div>
+        <div class="main-container">
+            <div class="main wrapper clearfix">
+
         <?php
-        if (isset($_GET['error'])) {
-            echo '<p class="error">Error Logging In!</p>';
-            
-            
-        }
+        if (isset($_GET['error'])) {echo '<p class="error">Error Logging In!</p>';}
         ?> 
-        <form action="includes/process_login.php" method="post" name="login_form"> 			
-            Email: <input type="e-mail" name="email" />
-            Password: <input type="password" 
-                             name="password" 
-                             id="password"/>
-            <input type="button" 
-                   value="Login" 
-                   onclick="formhash(this.form, this.form.password);" /> 
-         <div class="g-recaptcha" data-sitekey="6Le78QwTAAAAAIc1higa_X7ffM9-Oict99TtLTJr"></div>
-        </form>
-        <p>If you don't have a login, please <a href="register.php">register</a></p>
-        <p>If you are done, please <a href="includes/logout.php">log out</a>.</p>
-        <p>You are currently logged <?php echo $logged ?>.</p>
         
+        <form action="includes/process_login.php" method="post" name="login_form"> 			
+            <div class="input"><input type="e-mail" name="email" placeholder="E-mail" /></div>
+            <div class="input"><input type="password" name="password" id="password" placeholder="Password"/></div>
+            <div class="g-recaptcha" data-sitekey="6Le78QwTAAAAAIc1higa_X7ffM9-Oict99TtLTJr" data-theme="dark"></div>            
+            <input type="button" value="Login" onclick="formhash(this.form, this.form.password);" /> 
+        </form>
+
+            </div> <!-- #main -->
+        </div> <!-- #main-container -->
+
+        <div class="footer-container">
+            <footer class="wrapper">
+                <h3>Gemaakt door Vincent</h3>
+            </footer>
+        </div>
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
+
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
     </body>
 </html>
 
